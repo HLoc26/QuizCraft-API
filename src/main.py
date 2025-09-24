4b7320d80bbddeb73fe70981b7613cb09fb989ad
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-09-24 12:45:51 +0700

    feat: add CORS middleware configuration to main application

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from utils import ResponseHelper
 from routers import FilesRouter, MCQRouter
 
@@ -12,5 +12,16 @@ async def get_root():
     return ResponseHelper.success({"success": True})
 
 
+origins = ["http://localhost:5173"]
+methods = ["GET", "POST", "DELETE"]
+headers = ["Content-Type", "Authorization"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=methods,
+    allow_headers=headers,
+)
 app.include_router(FilesRouter().router, prefix="/files", tags=["files"])
 app.include_router(MCQRouter().router, prefix="/mcq", tags=["multiple_choice_question"])
